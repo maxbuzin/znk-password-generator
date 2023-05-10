6d9118c33905360cb147db31e3c9e62eb4275cf2
Max <maxbuzin@proton.me>
2023-05-10 10:05:56 +0400

    Password Generator v0.0.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,137 @@
-import Image from 'next/image'
+/** @format */
+'use client'
+
+import { useState } from 'react'
+import copy from 'copy-to-clipboard'
+
+const lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+const uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const numbers = '0123456789'
+const symbols = '!@#$%^&*()_-+='
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [passwordLength, setPasswordLength] = useState<number>(12)
+	const [includeLowercaseLetters, setIncludeLowercaseLetters] = useState<boolean>(true)
+	const [includeUppercaseLetters, setIncludeUppercaseLetters] = useState<boolean>(true)
+	const [includeNumbers, setIncludeNumbers] = useState<boolean>(true)
+	const [includeSymbols, setIncludeSymbols] = useState<boolean>(false)
+	const [generatedPassword, setGeneratedPassword] = useState<string>('')
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const generatePassword = () => {
+		let characters = ''
+		if (includeLowercaseLetters) {
+			characters += lowercaseLetters
+		}
+		if (includeUppercaseLetters) {
+			characters += uppercaseLetters
+		}
+		if (includeNumbers) {
+			characters += numbers
+		}
+		if (includeSymbols) {
+			characters += symbols
+		}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		let password = ''
+		for (let i = 0; i < passwordLength; i++) {
+			password += characters[Math.floor(Math.random() * characters.length)]
+		}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		setGeneratedPassword(password)
+	}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+	const handleCopy = () => {
+		copy(generatedPassword)
+	}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+	return (
+		<main className='flex flex-col items-center justify-center w-full min-h-screen text-zinc-100 bg-zinc-800'>
+			<div
+				id='content'
+				className='flex flex-col items-center justify-center w-full max-w-md'>
+				<div className=''>
+					<h2 className='text-2xl font-semibold text-center uppercase'>Password Generator</h2>
+				</div>
+				<div className='flex items-center justify-between w-full max-w-xs px-8 pt-8'>
+					<label className='text-zinc-100'>Password Length:</label>
+					<input
+						type='number'
+						min={8}
+						max={32}
+						value={passwordLength}
+						onChange={(event) => setPasswordLength(Number(event.target.value))}
+						className='flex px-3 py-2 leading-tight border rounded appearance-none max-w-max text-zinc-800 focus:outline-none focus:shadow-outline'
+					/>
+				</div>
+
+				<section className='flex flex-col items-center justify-between w-full max-w-xs gap-4 py-8'>
+					<div className='flex justify-between w-full px-8'>
+						<label className=''>Include Lowercase Letters:</label>
+						<input
+							type='checkbox'
+							checked={includeLowercaseLetters}
+							onChange={(event) => setIncludeLowercaseLetters(event.target.checked)}
+							className='w-5 h-5 text-gray-600 form-checkbox'
+						/>
+					</div>
+
+					<div className='flex justify-between w-full px-8'>
+						<label>Include Uppercase Letters:</label>
+						<input
+							type='checkbox'
+							checked={includeUppercaseLetters}
+							onChange={(event) => setIncludeUppercaseLetters(event.target.checked)}
+							className='w-5 h-5 text-gray-600 form-checkbox'
+						/>
+					</div>
+
+					<div className='flex justify-between w-full px-8'>
+						<label>Include Numbers:</label>
+						<input
+							type='checkbox'
+							checked={includeNumbers}
+							onChange={(event) => setIncludeNumbers(event.target.checked)}
+							className='w-5 h-5 text-gray-600 form-checkbox'
+						/>
+					</div>
+
+					<div className='flex justify-between w-full px-8'>
+						<label>Include Symbols:</label>
+						<input
+							type='checkbox'
+							checked={includeSymbols}
+							onChange={(event) => setIncludeSymbols(event.target.checked)}
+							className='w-5 h-5 text-gray-600 form-checkbox'
+						/>
+					</div>
+				</section>
+				<div>
+					<button
+						onClick={generatePassword}
+						className='px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline'>
+						Generate Password
+					</button>
+				</div>
+
+				{generatedPassword && (
+					<div className='flex flex-col justify-center w-full max-w-sm mt-8'>
+						<label className='block mb-2 text-center'>Generated Password:</label>
+						<div className='flex flex-col items-center gap-4'>
+							<input
+								type='text'
+								readOnly
+								value={generatedPassword}
+								className='w-full px-3 py-2 font-mono font-bold text-center text-gray-800 bg-gray-200 rounded'
+							/>
+							<button
+								onClick={handleCopy}
+								className='px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline'>
+								Copy
+							</button>
+						</div>
+					</div>
+				)}
+			</div>
+		</main>
+	)
 }
